4eb62027bb40de225cc638101be7136ff7ff1df6
rohannegi484@gmail.com <rohannegi484@gmail.com>
2025-09-24 00:42:25 +0530

    completed the clerk auth icon setup

---
+++ src/app/ai/components/ChatLabel.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ChatLabel = () => {
+  return (
+    <div className='flex items-center justify-between p-2 text-white/80 hover:bg-white/10 rounded-lg text-sm group cursor-pointer'>
+        <p className=''>
+
+        </p>
+    </div>
+  )
+}
+
+export default ChatLabel
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+
+import {AppContextProvider} from "../../src/app/ai/context/AppContext"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,13 +26,16 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-<html lang="en">
+      <AppContextProvider>
+        <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
     </html>
+      </AppContextProvider>
+
 
     </ClerkProvider>
     
