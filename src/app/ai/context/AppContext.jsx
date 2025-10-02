35c8090a8c5d78149dad80c53cd13bbbacb25590
rohannegi484@gmail.com <rohannegi484@gmail.com>
2025-10-02 13:58:53 +0530

    setting up the responses

--- src/app/ai/context/AppContext.jsx
+++ src/app/ai/context/AppContext.jsx
@@ -1,8 +1,9 @@
 "use client"
 
-import { useUser } from "@clerk/nextjs"
-import { createContext, useContext } from "react"
-
+import { useAuth, useUser } from "@clerk/nextjs"
+import { createContext, useContext, useState } from "react"
+import axios from "axios"
+import toast from "react-hot-toast"
 export const AppContext = createContext()
 
 export const useAppContext = () =>{
@@ -11,7 +12,52 @@ export const useAppContext = () =>{
 
 export const AppContextProvider = ({children}) => {
     const { user } = useUser()
+    const {getToken} = useAuth()
 
+    const [chats, setChats] = useState([])
+    const [selectedChat, setSelectedChat] = useState(null)
+
+    const createNewChat = async ()=>{
+        try {
+            if(!user) return null
+            const token = await getToken()
+
+            await axios.post("/api/chat/create" ,{ } , {
+                headers : {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    const fetchUsersChats = async()=>{
+        try {
+            const token = await getToken()
+            const {data} = await axios.get('/api/chat/get', {
+                headers:{
+                    Authorization: `Bearer ${token}`
+                }
+            })
+
+            if(data.success){
+                console.log(data.data)
+                setChats(data.data)
+
+
+                if(data.data.length === 0){
+                    await createNewChat();
+                    return fetchUsersChats()
+                }
+            }
+            else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
     const value = {
         user
     }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-
+import {Toaster } from "react-hot-toast"
 import {AppContextProvider} from "../../src/app/ai/context/AppContext"
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +31,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster
+          toastOptions={
+            {
+              success: {style:{background: "black", color:"white"}},
+              error: {style:{background: "black", color:"white"}}
+            }
+          }
+          ></Toaster>
         {children}
       </body>
     </html>
